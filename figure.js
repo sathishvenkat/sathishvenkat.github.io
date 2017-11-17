8424c63e78a7ec6d8d6d1ed7210114ec7fc213fd
Sathishkumar Venkatachalam <sathishkumar.525@gmail.com>
2017-11-17 22:48:35 +0530

    f.js

---
+++ figure.js
@@ -0,0 +1,32 @@
+var figure = {
+    "frames": [], 
+    "layout": {"autosize":false,"hovermode":"closest","height":625,"width":1000,"margin":{"t":25},"xaxis":{"domain":[0,0.6],"autorange":false,"range":[1.0395635,3.8628575000000005],"title":"Wafer Location X","type":"linear","anchor":"y1"},"yaxis":{"domain":[0,1],"autorange":false,"range":[1.0632916000000001,3.8939644],"title":"Wafer Location Y","type":"linear","anchor":"x1"},"xaxis4":{"domain":[0.75,1],"autorange":false,"range":[13.82,17.78],"anchor":"y4"},"xaxis5":{"title":"Mean:15.9  3sig:1.4  Range:3.8","titlefont":{"color":"white","size":"11"},"tickfont":{"color":"white"},"anchor":"y4","overlaying":"x4","side":"top"},"yaxis4":{"domain":[0.6,1],"autorange":false,"range":[4,7134],"anchor":"x4","showticklabels":false},"yaxis5":{"domain":[0.6,1],"autorange":false,"range":[0,0.9101221752300414],"anchor":"x4","overlaying":"y4","showticklabels":false},"showlegend":false}, 
+    "data": [
+        {
+            "x":[1.299341,1.299341,1.3478970000000001],
+            "y":[1.32556,1.32556,1.324732],
+            "z":[15.645348,14.483781,15.951885],
+            "autocolorscale":false,
+            "colorbar":{"x":0.6,"y":0.5,"title":"(nm)"},
+            "colorscale":[["0","rgb(209,0,0)"],["0.16667","rgb(255,102,34)"],["0.33333","rgb(255,218,33)"],["0.5","rgb(51,221,0)"],["0.66667","rgb(17,51,204)"],["0.83333","rgb(102,0,240)"],["1","rgb(238,0,238)"]],
+            "name":"Col3",
+            "reversescale":true,
+            "hoverinfo":"none",
+            "type":"heatmap",
+            "zsmooth":"best",
+            "uid":"766f76",
+            "zmax":17.62,
+            "zmin":13.82
+        },{
+            "x":[1.299341,1.299341,1.3478970000000001],
+            "y":[1.32556,1.32556,1.324732,1.324777],
+            "text":["radius=0.002<br>value=15.645348","radius=0.002<br>value=14.483781","radius=0.002<br>value=15.951885"],
+            "textposition":"top",
+            "textfont":{"color":"rgb(0, 0, 0)","size":12},
+            "mode":"markers",
+            "type":"scatter",
+            "marker":{"size":3,"color":["#000","#000","#000","#000","#000"]},
+            "hoverinfo":"x+y+text"
+        },{
+            "x":[13.82,14.14,14.46,14.780000000000001,15.100000000000001,15.420000000000002,15.740000000000002,16.060000000000002,16.380000000000003,16.700000000000003,17.020000000000003,17.340000000000003],"y":[4,30,171,818,2224,4532,5945,4835,2351,621,81,5],"mode":"markers","type":"bar","xaxis":"x4","yaxis":"y4","fill":"tozeroy","marker":{"size":1,"color":["#000","#000","#000","#000","#000"]},"hoverinfo":"x+y"},{"x":[13.5,17.78],"y":[7134,7134],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"black","mode":"none","hoverinfo":"none"},{"x":[13.82,14.14],"y":[4,4],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"rgb(203.64279240730974,0,238.5052530528337)","mode":"none","hoverinfo":"none"},{"x":[14.14,14.46],"y":[30,30],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"rgb(134.92837722192917,0,239.51575915850106)","mode":"none","hoverinfo":"none"},{"x":[14.46,14.780000000000001],"y":[171,171],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"rgb(79.63238424273806,13.420569454357171,230.5266568557479)","mode":"none","hoverinfo":"none"},{"x":[14.780000000000001,15.100000000000001],"y":[818,818],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"rgb(36.68329785823002,39.19002128506199,212.33645556348566)","mode":"none","hoverinfo":"none"},{"x":[15.100000000000001,15.420000000000002],"y":[2224,2224],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"rgb(26.30575704275394,97.5287852137697,148.16545774347637)","mode":"none","hoverinfo":"none"},{"x":[15.420000000000002,15.740000000000002],"y":[4532,4532],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"rgb(43.484360839099075,183.42180419549538,45.09383496540556)","mode":"none","hoverinfo":"none"},{"x":[15.740000000000002,16.060000000000002],"y":[5945,5945],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"rgb(108.97778781266526,220.14738547334315,9.378759793225262)","mode":"none","hoverinfo":"none"},{"x":[16.060000000000002,16.380000000000003],"y":[4835,4835],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"rgb(212.04941059073607,218.63162631484212,26.052110536736716)","mode":"none","hoverinfo":"none"},{"x":[16.380000000000003,16.700000000000003],"y":[2351,2351],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"rgb(255,183.81147877494016,33.294728631250514)","mode":"none","hoverinfo":"none"},{"x":[16.700000000000003,17.020000000000003],"y":[621,621],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"rgb(255,125.19860794431736,33.800012000480024)","mode":"none","hoverinfo":"none"},{"x":[17.020000000000003,17.340000000000003],"y":[81,81],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"rgb(240.9572555917301,70.86174065992326,23.62058021997442)","mode":"none","hoverinfo":"none"},{"x":[17.340000000000003,17.660000000000004],"y":[5,5],"type":"scatter","xaxis":"x4","yaxis":"y4","fill":"tozeroy","fillcolor":"rgb(217.7156151613808,19.325929270887855,6.441976423629285)","mode":"none","hoverinfo":"none"},{"x":[13.82,14.14,14.46,14.780000000000001,15.100000000000001,15.420000000000002,15.740000000000002,16.060000000000002,16.380000000000003,16.700000000000003,17.020000000000003,17.340000000000003,17.660000000000004],"y":[0.00002920642683710279,0.0005515004227447687,0.0063679778133505375,0.044961987827262774,0.19412333149646785,0.5125051553680554,0.8273837956636739,0.8167780418551633,0.4930481875181408,0.18199646380759318,0.04107946727554834,0.0056698938256721735,0.0004785345729445298],"type":"scatter","xaxis":"x4","yaxis":"y5","line":{"color":"white"},"mode":"lines","hoverinfo":"none"}]
+}
